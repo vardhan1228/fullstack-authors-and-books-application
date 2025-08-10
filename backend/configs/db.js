20b3f8cdcce4b81c744434d0b10c3b9df26463cb
srivardhan <159407504+vardhan1228@users.noreply.github.com>
2025-08-10 12:10:17 +0530

    Update db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,11 +1,23 @@
+// const mysql = require('mysql2');
+
+// const db = mysql.createConnection({
+//    host: 'localhost',
+//    port: '3306',
+//    user: 'root',
+//    password: '12345678',
+//    database: 'react_node_app'
+// });
+
+// module.exports = db;
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-   host: 'localhost',
-   port: '3306',
-   user: 'root',
-   password: '12345678',
-   database: 'react_node_app'
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT,
+   user: process.env.DB_USERNAME,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME
 });
 
 module.exports = db;
